2eabaff445c1b4532b478921b45c2b63d2e717f1
Michał Jarząbek <nebraska994@gmail.com>
2017-08-04 10:06:36 +0200

    finish section six

--- app/js/main.js
+++ app/js/main.js
@@ -1,112 +1,145 @@
 "use strict";
-$(function(){
+$(function () {
 	console.log("jestem");
 	/*Zmienne*/
 	var slideCount = $(".single-slide").length;
 	var slideShow = $(".slide-show");
-	var slideWidth = 100/slideCount;//lub =$(single-slide)
+	var slideWidth = 100 / slideCount; //lub =$(single-slide)
 	var slideIndex = 0;
-	
-	
+
+
 	/*Szerokość kontenera*/
 	slideShow.css("width", slideCount * 100 + "%");
-//	console.log(slideCount);
-	
-	/*Szerokość slaidu i jego połozenie (margines miedzy nimi)*//*Moje!!! dobrze*/
-	//100% podzielić przez ilość stajdów i to będzie szerokość jednego obrazka
-	/*slideWidth.each(function () {
-	   slideWidth.css({"width", 100/ slideCount + "%","margin-left": index * slideWidth + "%"  })});
-		*/
-	
-		/*lub*/
-		
-/*	Szerokosc slaidu i jego położenie*/	
-		$(".single-slide").each(function (index){
-//			console.log("test");
-//			console.log("index");
-			$(this).css({"width": slideWidth + "%", "margin-left": index * slideWidth + "%" });
+	//	console.log(slideCount);
+
+
+
+	/*	Szerokosc slaidu i jego położenie*/
+	$(".single-slide").each(function (index) {
+		//			console.log("test");
+		//			console.log("index");
+		$(this).css({
+			"width": slideWidth + "%",
+			"margin-left": index * slideWidth + "%"
 		});
-		
-	
+	});
+
+
 	/*Funkcjia slide*/
 	function slide(newSlideIndex) {
-		
+
 		console.log(newSlideIndex);
-		
-		if(newSlideIndex < 0 || newSlideIndex >= slideCount) {
+
+		if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
 			return;
 		}
-		
+
 		var marginLeft = (newSlideIndex * (-100)) + "%";
-		slideShow.animate({"margin-left": marginLeft},800,function() {
+		slideShow.animate({
+			"margin-left": marginLeft
+		}, 800, function () {
 			slideIndex = newSlideIndex;
 		});
-		
+
 	};
-		
+
 	/*Klikniecie strzałki*/
-	$(".prev-slide").click(function() {
+	$(".prev-slide").click(function () {
 		event.preventDefault();
 		slide(slideIndex - 1);
-		
-		/*Przeniesione do funkcji na górze i połączenie z drugą strzałką*/
-		/*var newSlideIndex = slideIndex - 1;*/
-	
-		/*Zatrzymanie funkcji przewijania slidów*/
-	/*	if(newSlideIndex < 0) {
-				return;
-		};
-		
-		var marginLeft = (newSlideIndex * (-100)) + "%";
-			
-			slideShow.animate({"margin-left": marginLeft},800,function() {
-				
-				
-			slideIndex = newSlideIndex;
-				
-			console.log(marginLeft);
-		*/
-		
-			
-		});
-		
-//	});
-	
-	$(".next-slide").click(function() {
-		
+
+
+
+
+	});
+
+
+
+	$(".next-slide").click(function () {
+
 		event.preventDefault();
 		console.log('newSlideIndex');
-		slide( slideIndex + 1);
-		
-		
-		/*Przeniesione do funkcji na górze i połączenie z drugą strzałką*/
-		/*var newSlideIndex = slideIndex + 1;
-		
-//		Zatrzymanie funkcji przewijania slidów
-		if(newSlideIndex >= slideCount) {
-				return;
-		};
-		
-		var marginLeft = (newSlideIndex * (-100)) + "%";
-		
-		slideShow.animate({"margin-left":marginLeft},800,function() {
-			slideIndex = newSlideIndex;
-		
-		console.log(marginLeft);*/
-			
-			
+		slide(slideIndex + 1);
+
+
+
+
+
+	});
+
+
+
+});
+
+$(function () {
+	console.log("jestem");
+	/*Zmienne*/
+	var slideCount = $(".single-slide-opinie").length;
+	var slideShow = $(".slide-show-opinie");
+	var slideWidth = 100 / slideCount; //lub =$(single-slide)
+	var slideIndex = 0;
+
+
+	/*Szerokość kontenera*/
+	slideShow.css("width", slideCount * 100 + "%");
+	//	console.log(slideCount);
+
+
+
+	/*	Szerokosc slaidu i jego położenie*/
+	$(".single-slide-opinie").each(function (index) {
+		//			console.log("test");
+		//			console.log("index");
+		$(this).css({
+			"width": slideWidth + "%",
+			"margin-left": index * slideWidth + "%"
 		});
-		
-//	});
-	
+	});
+
+
+	/*Funkcjia slide*/
+	function slide(newSlideIndex) {
+
+		console.log(newSlideIndex);
+
+		if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
+			return;
+		}
+
+		var marginLeft = (newSlideIndex * (-100)) + "%";
+		slideShow.animate({
+			"margin-left": marginLeft
+		}, 800, function () {
+			slideIndex = newSlideIndex;
+		});
+
+	};
+
 	/*Klikniecie strzałki*/
-	
-	
-	
-	
-	
-	/*Przesyunięcie slaidu -obsługa eventu , onclick strzałka w lewo i prawo*/
-	/*W momęcie kliknięcia strzałki index zwiększ się o 1, margin mnożymy, parametr + 1 , przesunięcie 0-100%*/
-	
+	$(".prev-slide").click(function () {
+		event.preventDefault();
+		slide(slideIndex - 1);
+
+
+
+
+	});
+
+
+
+	$(".next-slide").click(function () {
+
+		event.preventDefault();
+		console.log('newSlideIndex');
+		slide(slideIndex + 1);
+
+
+
+
+
+	});
+
+
+
 });
 
