0b2dbbfdd465bc297d07cca51ebfddb1a519e2e3
Michał Jarząbek <nebraska994@gmail.com>
2017-08-07 14:36:02 +0200

    walidacja formularza

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 "use strict";
 $(function () {
-	console.log("jestem");
+	//	console.log("jestem");
 	/*Zmienne*/
 	var slideCount = $(".single-slide").length;
 	var slideShow = $(".slide-show");
-	var slideWidth = 100 / slideCount; //lub =$(single-slide)
+	var slideWidth = 100 / slideCount;
 	var slideIndex = 0;
 
 
@@ -28,7 +28,7 @@ $(function () {
 	/*Funkcjia slide*/
 	function slide(newSlideIndex) {
 
-		console.log(newSlideIndex);
+		//		console.log(newSlideIndex);
 
 		if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
 			return;
@@ -72,11 +72,11 @@ $(function () {
 });
 
 $(function () {
-	console.log("jestem");
+	//	console.log("jestem");
 	/*Zmienne*/
 	var slideCount = $(".single-slide-opinie").length;
 	var slideShow = $(".slide-show-opinie");
-	var slideWidth = 100 / slideCount; //lub =$(single-slide)
+	var slideWidth = 100 / slideCount;
 	var slideIndex = 0;
 
 
@@ -100,7 +100,7 @@ $(function () {
 	/*Funkcjia slide*/
 	function slide(newSlideIndex) {
 
-		console.log(newSlideIndex);
+		//		console.log(newSlideIndex);
 
 		if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
 			return;
@@ -143,3 +143,61 @@ $(function () {
 
 });
 
+var zgoda = document.getElementById("zgoda");
+
+var inputImie = document.getElementById("imie");
+
+var inputNazwisko = document.getElementById("nazwisko");
+
+var inputEmail = document.getElementById("email");
+
+var pobierzBtn = document.getElementById("wyslij");
+
+var wiadomosc = document.getElementById("wiadomosc");
+
+
+function walidujFormularz(event) {
+	console.log("jestem w funkcji");
+	//	wiadomosc.innerHTML = "<ul id="wiadomosc"></ul>";
+
+
+	if (inputEmail.value.trim() == "") {
+		var msgEmail = document.createElement("li");
+		msgEmail.innerHTML = "Wpisz Email!";
+		wiadomosc.appendChild(msgEmail);
+
+		inputEmail.style.backgroundColor = "red";
+		event.preventDefault();
+	}
+
+	if (inputImie.value.trim() == "") {
+		var msgImie = document.createElement("li");
+		msgImie.innerHTML = "Wpisz Imie!";
+		wiadomosc.appendChild(msgImie);
+		inputImie.style.backgroundColor = "red";
+
+		event.preventDefault();
+	}
+
+	if (inputNazwisko.value.trim() == "") {
+		var msgNazwisko = document.createElement("li");
+		msgNazwisko.innerHTML = "Wpisz Nazwisko!";
+		wiadomosc.appendChild(msgNazwisko);
+		inputNazwisko.style.backgroundColor = "red";
+
+		event.preventDefault();
+	}
+
+//	if (!zgoda.checked) {
+//		var msgZgoda = document.createElement("li");
+//		msgZgoda.innerHTML = "Musisz wyrazić zgode!";
+//		wiadomosc.appendChild(msgZgoda);
+//
+//		event.preventDefault();
+//	}
+
+
+
+}
+
+pobierzBtn.addEventListener("click", walidujFormularz);
